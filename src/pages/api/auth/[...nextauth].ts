61115363a896f2cf2cc730af0e256e5c5b41ea0d
Key Yu Wan <keyflcbyuwan@gmail.com>
2021-11-25 23:39:06 -0300

    next auth callback - signIn

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,12 +1,29 @@
-import NextAuth from "next-auth"
-import Providers from "next-auth/providers"
+import NextAuth from 'next-auth'
+import Providers from 'next-auth/providers'
+import { query as q } from 'faunadb'
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
   providers: [
     Providers.GitHub({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
-      scope: "read:user",
+      scope: 'read:user',
     }),
   ],
+  callbacks: {
+    // salvo no db quando user fizer signIn
+    async signIn(user, account, profile) {
+      const { email } = user
+
+      try {
+        await fauna.query(q.Create(q.Collection('users'), { data: { email } }))
+
+        return true
+      } catch (err: any) {
+        console.log(err)
+        return false
+      }
+    },
+  },
 })
