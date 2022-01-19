99ff534b9398a301e15b1edbab62dcbabd3590a8
Key Yu Wan <keyflcbyuwan@gmail.com>
2022-01-19 19:56:31 -0300

    Test configs

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  testIgnorePatterns: ['/node_modules/', '/.next/'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
+  },
+  // arquivos que o jest executa antes de executar os testes
+  setupFilesAfterEnv: ['<rootDir>/src/tests/setupTests.ts'],
+  testEnvironment: 'jsdom',
+}
